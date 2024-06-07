dac2e78b38a0c79ea5c2cacbe524c0550cdf8d61
Andrei Chabatul <andreichabatul@gmail.com>
2024-06-07 11:39:44 +0300

    feat: add lesson component

--- sirius-future/src/app/(MainPage)/components/lessonsSection/components/itemLesson.tsx
+++ sirius-future/src/app/(MainPage)/components/lessonsSection/components/itemLesson.tsx
@@ -2,18 +2,9 @@ import styles from './itemLesson.module.scss';
 import { useDateLesson } from "@/hooks/useDateLesson"
 import { infoLesson } from "@/types/itemLesson";
 
-export default function ItemLesson() {
+export default function ItemLesson({ date, duration, name, teacher }: infoLesson) {
 
-
-    const itemLesson1d: infoLesson = {
-        name: 'Ментальная Арифметика',
-        teacher: 'Белкина Инна',
-        duration: 300000,
-        date: "December 17, 1995 13:24:00"
-
-    }
-
-    const { timeLesson, day, month } = useDateLesson(itemLesson1d.date, itemLesson1d.duration);
+    const { timeLesson, day, month } = useDateLesson(date, duration);
 
     return (
         <li className={styles.wrapper}>
@@ -21,16 +12,16 @@ export default function ItemLesson() {
                 <span className={styles.day}>{day}</span>
                 <span className={styles.month}>{month}</span>
             </div>
-            <span className={styles.name} >{itemLesson1d.name}</span>
+            <span className={styles.name} >{name}</span>
             <span className={styles.duration}>{timeLesson}</span>
             <p className={styles.teacher}>
                 <span className='material-symbols-outlined' style={{ 'fontSize': '18px', 'color': '#79747F' }}>account_circle</span>
-                {itemLesson1d.teacher}
+                {teacher}
             </p>
 
-            <div>
-                <button>Кнопка</button>
-                <button>Кнопка</button>
+            <div className='flex gap-1'>
+                <button className={styles.button + ' ' + styles.buttonWhite}>Кнопка</button>
+                <button className={styles.button + ' ' + styles.buttonBlue}>Кнопка</button>
             </div>
         </li>
     )
--- sirius-future/src/app/(MainPage)/components/lessonsSection/lessonsSection.tsx
+++ sirius-future/src/app/(MainPage)/components/lessonsSection/lessonsSection.tsx
@@ -1,11 +1,41 @@
+import { infoLesson } from '@/types/itemLesson';
 import ItemLesson from './components/itemLesson';
 import styles from './lessonSection.module.scss';
+
+
+const itemLesson1d: infoLesson[] = [
+    {
+        name: 'Ментальная Арифметика',
+        teacher: 'Белкина Инна',
+        duration: 300000,
+        date: "December 17, 1995 13:24:00"
+
+    },
+    {
+        name: 'Программирование',
+        teacher: 'Животновская Оксана',
+        duration: 3000000,
+        date: "April 1, 1995 12:15:00"
+
+    },
+    {
+        name: 'Изучение React',
+        teacher: 'Владислав Игорев',
+        duration: 10000000,
+        date: "March 4, 1995 9:00:00"
+
+    }]
+
 
 export default function LessonsSections() {
     return (
         <section className={styles.wrapper}>
             <h6 className={styles.title}>Ближайшие уроки</h6>
-            <ItemLesson/>
+            <ul className='mb-7'>
+                {itemLesson1d.map((lesson) =>
+                    <ItemLesson key={lesson.name} {...lesson} />)}
+            </ul>
+            <button className={styles.button}>Кнопка</button>
         </section>
     )
 }
